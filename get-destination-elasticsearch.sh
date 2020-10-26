0d221d124c651610ee853baf60e9d76e8b49e2c1
Michael Marcotte <mike.marcotte@gmail.com>
2020-10-25 22:38:06 -0400

    fix script that determines eas name

--- get-destination-elasticsearch.sh
+++ get-destination-elasticsearch.sh
@@ -13,7 +13,7 @@
 ENV=$1
 
 DESTINATION_TABLE_VERSION=$(aws dynamodb get-item --region us-east-1 --table-name "efcms-deploy-${ENV}" --key '{"pk":{"S":"destination-table-version"},"sk":{"S":"destination-table-version"}}' | jq -r ".Item.current.S")
-[ -z "$DESTINATION_TABLE_VERSION" ] && echo "efcms-search-${ENV}"
+[ -z "$DESTINATION_TABLE_VERSION" ] && echo "efcms-search-${ENV}" && exit
 
 echo "efcms-search-${ENV}-${DESTINATION_TABLE_VERSION}"
 
